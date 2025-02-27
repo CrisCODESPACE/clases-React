03dc6240f500d7e66ce4fe2e3d584ac334cb2d20
ElenaDelHer <medelh1992@gmail.com>
2025-02-27 13:37:48 +0000

    add header

---
+++ TiendaApple/src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header =()=>{
+    return(
+        <div>
+        <img src="https://th.bing.com/th/id/OIP.pW0TOZyl_OZMIuo5HjUsyQHaIf?rs=1&pid=ImgDetMain" alt="Logo" />
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Catálogo</li>
+                <li>Carrito</li>
+            </ul>
+        </nav>
+        </div>
+    )
+}
+export default Header;
